4f973bf37c91f0ea9d2040cbe85855fa14c9464c
you@example.com <you@example.com>
2021-08-13 15:17:49 +0530

    msg

--- games/views.py
+++ games/views.py
@@ -3,5 +3,5 @@ from .models import Game, GameResult
 
 def game(request, gameid):
     game = Game.objects.get(id=gameid)
-    results = GameResult.objects.filter(game=game)
+    results = GameResult.objects.filter(game=game).order_by('-start_time')
     return render(request, 'games/game.html', {'game': game, 'results': results})

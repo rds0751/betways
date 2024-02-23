124183c0e8c7218f49d0ddcc437a6bc0a9d489ae
ripudaman <ripudaman@asynchain.com>
2024-02-23 13:11:16 +0530

    msg

--- casino/App.js
+++ casino/App.js
@@ -1,20 +1,81 @@
-import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useRef, useEffect } from 'react';
+import { ActivityIndicator, Linking, SafeAreaView, StyleSheet, BackHandler } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
 
 export default function App() {
-  return (
-    <WebView
-      style={styles.container}
-      originWhitelist={['*']}
-      source={{ uri: 'https://crown.0x360.com/users/' }}
-    />
-  );
-}
+    const webViewRef = useRef()
+    const [isLoadong, setLoading] = useState(false);
 
+    const handleBackButtonPress = () => {
+        try {
+            webViewRef.current?.goBack()
+        } catch (err) {
+            console.log("[handleBackButtonPress] Error : ", err.message)
+        }
+    }
+
+    useEffect(() => {
+        BackHandler.addEventListener("hardwareBackPress", handleBackButtonPress)
+        return () => {
+            BackHandler.removeEventListener("hardwareBackPress", handleBackButtonPress)
+        };
+    }, []);
+
+    return (
+      <SafeAreaView style={styles.safeArea}>
+        <WebView
+            originWhiteList={['*']}
+            source={{ uri: 'https://crown.0x360.com/users/' }}
+            style={styles.container} 
+            ref={webViewRef}
+            onLoadStart={(syntheticEvent) => {
+                setLoading(true);
+            }}
+            onShouldStartLoadWithRequest={(event)=>{
+                if (event.navigationType === 'click') {
+                    if (!event.url.match(/(google\.com\/*)/) ) {
+                        Linking.openURL(event.url)
+                        return false
+                    }
+                    return true
+                }
+                else{
+                    return true;
+                }
+            }}
+            onLoadEnd={(syntheticEvent) => {
+                setLoading(false);
+            }}
+        />
+        {isLoadong && (
+            <ActivityIndicator
+            color="#234356"
+            size="large"
+            style={styles.loading}
+            />
+        )}
+        </SafeAreaView>
+      );
+  }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  safeArea: {
+  flex: 1,
+  backgroundColor: '#234356'
+},
+loading: {
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  alignItems: 'center',
+  justifyContent: 'center'
+},
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
 });
